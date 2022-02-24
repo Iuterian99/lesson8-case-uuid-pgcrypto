169710aeea9f2460964f7b69ab449fd9de79736c
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-02-24 23:53:37 +0500

    NICE JOB

--- src/model/model.sql
+++ src/model/model.sql
@@ -8,3 +8,19 @@ restaurant_id = branch_id
 GROUP BY restaurant_name;
 
 select * from restaurants where restaurant_name LIKE 'r%';
+
+CREATE EXTENSION "uuid-ossp";
+
+CREATE TABLE users(
+  user_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+  user_name varchar(50) NOT NULL
+);
+
+
+INSERT INTO users(user_name) values('Muslim');
+
+CREATE TABLE posts(
+  post_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+  post_title varchar(100) NOT NULL,
+  user_id uuid REFERENCES users(user_id)
+);
